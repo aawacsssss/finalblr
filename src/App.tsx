fc3ed45fbbba6b26ea034a706b4998d5dc0db946
Basri <basrican2000@gmail.com>
2025-07-29 21:52:11 +0300

    Yorum eklendi, test için

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import AppRoutesWithLayout from './AppRoutesWithLayout';
 import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
+  // Test amacıyla eklendi
+  console.log("App bileşeni çalıştı");
+  
   return (
     <AuthProvider>
       <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
